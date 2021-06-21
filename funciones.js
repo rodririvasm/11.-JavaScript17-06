5007e116dd83f6ebd8a9927e597f68a908d1308d
rodrigo <rodrigorivasmiranda@gmail.com>
2021-06-20 21:37:57 -0500

    actualizado

---
+++ funciones.js
@@ -0,0 +1,6 @@
+const nombre='rodrigo';
+const apellidos='rivas miranda';
+
+console.log(`hola mi nombre es ${nombre} y mis apellidos son ${apellidos}`);/*  ` `  salen con alt 96*/
+
+const arreglo=[1,2,3,4,5]
